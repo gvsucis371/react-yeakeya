c11792e76ac7a825f689aab59eab708e14caa3f0
Anderson Yeakey <yeakeya@gvsu.edu>
2025-03-20 16:46:19 -0400

    [Grade Me Part 1]

--- jsx_part1.jsx
+++ jsx_part1.jsx
@@ -1,12 +1,12 @@
 let buildings = ['Kirkhof', 'Padnos', 'Mackinac', 'Manitou', 'Niemeyer', 'Kindschi'];
     
-    const moreBuildings = <div>
-      <p>Here are some more buildings:</p>
-      <ul>
-        {buildings.map((building) => <li>{building}</li>)}
-      </ul>
-    </div>;
+const moreBuildings = <div>
+  <p>Here are some more buildings:</p>
+  <ul>
+    {buildings.map((building) => <li>{building}</li>)}
+  </ul>
+</div>;
     
-    const root = ReactDOM.createRoot(document.getElementById('react-list'));
-    root.render(moreBuildings);
+const root = ReactDOM.createRoot(document.getElementById('react-list'));
+root.render(moreBuildings);
     
